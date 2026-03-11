c2d6396d865949e09f48d65fa277d24e450264eb
Thomas Ahn <thomasahn2009@gmail.com>
2026-03-11 09:37:46 -0700

    added tagging

--- song/song.go
+++ song/song.go
@@ -9,30 +9,56 @@ import (
 	"github.com/gopxl/beep/flac"
 	"github.com/gopxl/beep/speaker"
 	"github.com/gopxl/beep/v2"
+	"go.senan.xyz/taglib"
 )
 
 type Song struct {
-	// album  string
-	// artist string
-	// title  string
-	// date   string
+	//tags
+	album       string
+	artist      string
+	title       string
+	date        string
+	trackNumber string
+	genre       []string
+
+	//properties
+	trackLength int
 
 	songPath string
-
-	// trackLength float32
-	// trackNumber int
-	// totalTracks int
-
-	// genre []string
 
 	// playing     bool
 	// in_playlist map[int]struct{}
 }
 
 func NewSong(path string) *Song {
+	tags, err := taglib.ReadTags(path)
+	if err != nil {
+		log.Fatalf("Error parsing metadata: %v", err)
+	}
+	properties, err := taglib.ReadProperties(path)
+	if err != nil {
+		log.Fatalf("Error parsing properties: %v", err)
+
+	}
+
 	return &Song{
+		album:       firstTag(tags, taglib.Album),
+		artist:      firstTag(tags, taglib.Artist),
+		title:       firstTag(tags, taglib.Title),
+		date:        firstTag(tags, taglib.ReleaseDate),
+		trackNumber: firstTag(tags, taglib.TrackNumber),
+		genre:       tags[taglib.Genre],
+		trackLength: int(properties.SampleRate),
+
 		songPath: path,
 	}
+}
+
+func firstTag(tags map[string][]string, key string) string {
+	if v, ok := tags[key]; ok && len(v) > 0 {
+		return v[0]
+	}
+	return ""
 }
 
 func (s Song) PlaySong() {
@@ -53,6 +79,10 @@ func (s Song) PlaySong() {
 	fmt.Printf("Sample Rate: %d Hz\n", format.SampleRate)
 	fmt.Printf("Channels:    %d\n", format.NumChannels)
 	fmt.Printf("Precision:   %d-bit\n", format.Precision*8)
+	fmt.Printf("Album:   %v\n", s.album)
+	fmt.Printf("Artist:   %v\n", s.artist)
+	fmt.Printf("Title:   %v\n", s.title)
+	fmt.Printf("Length:   %v\n", s.trackLength)
 
 	// Initialize the speaker with the file's sample rate
 	// Buffer size: 1/10th of a second
